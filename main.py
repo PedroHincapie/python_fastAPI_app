b172e8ed5237320511a6120ac8bf5d8d584d2acc
Pedro Jesus Hincapie Garcia <pedro.hincapie@cencosudscotiabank.cl>
2024-07-16 20:10:36 -0500

    Adicionar descripcion de la docs de swaager del proyecto in FastAPI

--- main.py
+++ main.py
@@ -2,7 +2,30 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+"""
+Se deseamos cambiar el título de la aplicación, podemos hacerlo con la propiedad title de la instancia de FastAPI.
+app.title = "My API with FastAPI"
+"""
+app.title = "My API with FastAPI"
 
-@app.get("/")
+
+"""
+Si deseamos adicionar alguna descripcion de proyecto tenemos
+la propiedad description de la instancia de FastAPI.
+app.description = "xXxxxxx"
+"""
+app.description = """
+Este es mi primer proyecto con FastAPI, dentro de este podemos hacer las
+prubeas de uso de la misma y aprender a componer API con FastAPI
+"""
+
+"""
+Si deseamos adicionar una versión a nuestra API, podemos hacerlo con la propiedad version de la instancia de FastAPI.
+app.version = "1.0.0"
+"""
+app.version = "1.0.0"
+
+
+@app.get("/", tags=["Root"])
 def read_root():
     return {"Hello": "World"}
